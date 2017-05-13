b0bca96e5fb6ced75d0432227899008c923486be
Uri Shaked <uri@salsa4fun.co.il>
2017-05-13 05:01:43 +0300

    BLEServer with scanning support

---
+++ BLEServer/BLEServer/BLEServer.cpp
@@ -0,0 +1,104 @@
+// BLEServer.cpp : Windows 10 Web Bluetooth Polyfill Server
+//
+// Copyright (C) 2017, Uri Shaked. License: MIT.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <Windows.Foundation.h>
+#include <Windows.Devices.Bluetooth.h>
+#include <Windows.Devices.Bluetooth.Advertisement.h>
+#include <Windows.Data.JSON.h>
+#include <wrl/wrappers/corewrappers.h>
+#include <wrl/event.h>
+#include <collection.h>
+#include <ppltasks.h>
+#include <string>
+#include <sstream> 
+#include <iomanip>
+#include <experimental/resumable>
+#include <pplawait.h>
+#include <cvt/wstring>
+#include <codecvt>
+
+using namespace Platform;
+using namespace Windows::Devices;
+using namespace Windows::Data::Json;
+
+std::wstring formatBluetoothAddress(unsigned long long BluetoothAddress) {
+	std::wostringstream ret;
+	ret << std::hex << std::setfill(L'0')
+		<< std::setw(2) << ((BluetoothAddress >> (5 * 8)) & 0xff) << ":"
+		<< std::setw(2) << ((BluetoothAddress >> (4 * 8)) & 0xff) << ":"
+		<< std::setw(2) << ((BluetoothAddress >> (3 * 8)) & 0xff) << ":"
+		<< std::setw(2) << ((BluetoothAddress >> (2 * 8)) & 0xff) << ":"
+		<< std::setw(2) << ((BluetoothAddress >> (1 * 8)) & 0xff) << ":"
+		<< std::setw(2) << ((BluetoothAddress >> (0 * 8)) & 0xff);
+	return ret.str();
+}
+
+void writeObject(JsonObject^ jsonObject) {
+	String^ jsonString = jsonObject->Stringify();
+
+	stdext::cvt::wstring_convert<std::codecvt_utf8<wchar_t>> convert;
+	std::string stringUtf8 = convert.to_bytes(jsonString->Data());
+
+	auto len = stringUtf8.length();
+	std::cout << char(len >> 0)
+		<< char(len >> 8)
+		<< char(len >> 16)
+		<< char(len >> 24);
+
+	std::cout << stringUtf8 << std::flush;
+}
+
+int main(Array<String^>^ args) {
+	Microsoft::WRL::Wrappers::RoInitializeWrapper initialize(RO_INIT_MULTITHREADED);
+
+	CoInitializeSecurity(
+		nullptr, // TODO: "O:BAG:BAD:(A;;0x7;;;PS)(A;;0x3;;;SY)(A;;0x7;;;BA)(A;;0x3;;;AC)(A;;0x3;;;LS)(A;;0x3;;;NS)"
+		-1,
+		nullptr,
+		nullptr,
+		RPC_C_AUTHN_LEVEL_DEFAULT,
+		RPC_C_IMP_LEVEL_IDENTIFY,
+		NULL,
+		EOAC_NONE,
+		nullptr);
+
+	Bluetooth::Advertisement::BluetoothLEAdvertisementWatcher^ bleAdvertisementWatcher = ref new Bluetooth::Advertisement::BluetoothLEAdvertisementWatcher();
+	bleAdvertisementWatcher->ScanningMode = Bluetooth::Advertisement::BluetoothLEScanningMode::Active;
+	bleAdvertisementWatcher->Received += ref new Windows::Foundation::TypedEventHandler<Bluetooth::Advertisement::BluetoothLEAdvertisementWatcher ^, Windows::Devices::Bluetooth::Advertisement::BluetoothLEAdvertisementReceivedEventArgs ^>(
+		[bleAdvertisementWatcher](Bluetooth::Advertisement::BluetoothLEAdvertisementWatcher ^watcher, Bluetooth::Advertisement::BluetoothLEAdvertisementReceivedEventArgs^ eventArgs) {
+		unsigned int index = -1;
+
+		JsonObject^ msg = ref new JsonObject();
+		msg->Insert("_type", JsonValue::CreateStringValue("ScanResult"));
+		msg->Insert("bluetoothAddress", JsonValue::CreateStringValue(ref new String(formatBluetoothAddress(eventArgs->BluetoothAddress).c_str())));
+		msg->Insert("rssi", JsonValue::CreateNumberValue(eventArgs->RawSignalStrengthInDBm));
+		// TODO fix timestamp calculation
+		msg->Insert("timestamp", JsonValue::CreateNumberValue((double)eventArgs->Timestamp.UniversalTime / 10000.0 + 11644480800000));
+		msg->Insert("advType", JsonValue::CreateStringValue(eventArgs->AdvertisementType.ToString()));
+		msg->Insert("localName", JsonValue::CreateStringValue(eventArgs->Advertisement->LocalName));
+
+		JsonArray^ serviceUuids = ref new JsonArray();
+		for (int i = 0; i < eventArgs->Advertisement->ServiceUuids->Size; i++) {
+			serviceUuids->Append(JsonValue::CreateStringValue(eventArgs->Advertisement->ServiceUuids->GetAt(i).ToString()));
+		}
+		msg->Insert("serviceUuids", serviceUuids);
+
+		// TODO manfuacturer data / flags / data sections ?
+		writeObject(msg);
+	});
+	bleAdvertisementWatcher->Start();
+	
+	JsonObject^ msg = ref new JsonObject();
+	msg->Insert("_type", JsonValue::CreateStringValue("Start"));
+	writeObject(msg);
+
+	while (1) {
+		getchar();
+	}
+
+	return 0;
+}
